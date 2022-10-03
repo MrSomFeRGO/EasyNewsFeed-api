477b3da31142bf80ded20700b7f01acd31f417c0
BuildTools <unconfigured@null.spigotmc.org>
2022-10-03 03:09:15 +0300

    init

---
+++ src/main/java/by/mrsomfergo/easynewsfeed/EasyNewsFeedApplication.java
@@ -0,0 +1,13 @@
+package by.mrsomfergo.easynewsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyNewsFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyNewsFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/mrsomfergo/easynewsfeed/EasyNewsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mrsomfergo.easynewsfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyNewsFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
